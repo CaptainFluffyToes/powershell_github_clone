f6549b2ef89df45e98bdd124e31b68c930aea011
CaptainFluffyToes <DJKhan85@gmail.com>
2017-12-21 09:14:13 -0500

    Added code for checking of the entered repositories.  Updated readme file.

--- README.md
+++ README.md
@@ -2,21 +2,20 @@
 ## Description
 This powershell function's main purpose is to keep a local directory updated with all of an accounts repositories.  It has some built in saftey features:
 
-* Clones all new repositories at the same level as any existing repository
-* Checks existing directories to make sure they are git repositories
-* Runs git pull --all on any existing repositories
+* Clones all new repositories at the same level as any existing repository.
+* Checks existing directories to make sure they are git repositories.
+* Runs git pull --all on any existing repositories.
+* Verifies than any passed through repository exists before attempting to clone/fetch changes.
+* Will terminate if the entered repository names do not match with any of the online names.
 
 It gathers the repositories by connecting to the github api to fetch a current listing of all repositories.
 
 ## Usage
-The .ps1 file is not self executing.  You will have to add 'GitHub-Clone' to the end of the file if you would like it to execute without any other commands.  otherwise you can run:
-
-* powershell "github-clone.ps1"
-
-The ps1 file can also be added to your user powershell file so that the cmdlet is always available.
+The .ps1 file is self executing.  Drop the file into the directory that you would like to use for your GitHub repositories.  They execute the .ps1 file from a powershell prompt.
 
 ## Dependencies
 
 * Powershell 3.0 or greater
 * Git tools
 * Internet Connection
+* GitHub Account
--- github_clone.ps1
+++ github_clone.ps1
@@ -5,6 +5,8 @@
    This cmdlet will get a full listing of repositories for the specified account by connecting to the GitHub api to pull down the repos.  It will then check the existing directory to see if there are any existing repositories and perform a git pull --all to fetch the latest changes.  It will clone any non existing repos into the same directory.
 .EXAMPLE
    GitHub-Clone -Account "CaptainFluffyToes"
+.EXAMPLE
+   GitHub-Clone -Account "CaptainFluffyToes" -User_Repos "docker_media_plex"
 .FUNCTIONALITY
    The functionality that best describes this cmdlet
 #>
@@ -23,61 +25,123 @@ function GitHub-Clone
                    ValueFromPipelineByPropertyName=$true, 
                    ValueFromRemainingArguments=$false, 
                    Position=0)]
-        [string]$Account
+        [string]$Account,
+
+        #This parameter is the list of repositories that has been specified to be pulled.
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=1)]
+        [array]$User_Repos
     )
 
     Begin
     {
+        #Gathering information about the GitHub account that we are going to use for the repo download
         do {
             $Account = Read-Host -Prompt 'Please enter the name of the GitHub Account.'
             if ($Account -eq $Null) {
                 Write-Host 'No account name entered!'
             }
         } while ($Account -eq $Null)
-        if ($Repos -eq $Null) {
+
+        #We will pull all of the repos from the account since no specific repo was specified
+        if ($User_Repos -eq $Null) {
             $Repos = Invoke-RestMethod -Method GET -Uri "https://api.github.com/users/$Account/repos"
+            $continue = $true
+        }
+
+        #Checking to make sure that the specified repo(s) exist in the GitHub account
+        elseif ($User_Repos -ne $Null) {
+            $Web_Repos = Invoke-RestMethod -Method GET -Uri "https://api.github.com/users/$Account/repos"
+            foreach ($User_Repo in $User_Repos) {
+                [array]$matched_repo = ($Web_Repos | Where-Object {($_.name -match "$User_Repo")})
+                if ($matched_repo -eq $Null) {
+                    Write-Host "Coudn't match $User_repo to the list of online repositories for $Account." -ForegroundColor Red -BackgroundColor White
+                }
+                else {
+                    [array]$Repos += $matched_repo
+                    Write-Host "Found matching repository $matched_repo for user entered repository $User_repo" -ForegroundColor Red -BackgroundColor White
+                }
+            }
+            if ($matched_repo -eq $Null) {
+                $continue = $false
+                Write-Host "No matching repos found for entered list." -ForegroundColor Red -BackgroundColor White
+            }
+            else {
+                $continue = $true
+            }
         }
     }
     Process
     {
-        [array]$existing_repos = Get-ChildItem -Directory
-        if ($existing_repos -eq $Null) {
-            $count = 1
-            foreach ($new_repo in $Repos) {
-                Write-Host "Creating new repository $($new_repo.name). Number $count of $(($Repos.name | Measure-Object).count)" -ForegroundColor Red
-                git clone "$($new_repo.clone_url)"
-                $count++
+        if ($continue -eq $true) {
+            #Getting list of existing directories that may or may not be git repos
+            [array]$existing_repos = Get-ChildItem -Directory
+
+            #Cloning all repos that are needed since no existing directories were found
+            if ($existing_repos -eq $Null) {
+                $count = 1
+                foreach ($new_repo in $Repos) {
+                    Write-Host "Creating new repository $($new_repo.name). Number $count of $(($Repos.name | Measure-Object).count)" -ForegroundColor Red -BackgroundColor White
+                    git clone "$($new_repo.clone_url)"
+                    $count++
+                }
             }
+
+            #Found existing directories that might be repos. 
+            else {
+
+                #Checking the name of the current potential repositories against the list of repos that we are trying to work with from the web.
+                $comparison = Compare-Object -ReferenceObject $existing_repos.name -DifferenceObject $Repos.name
+
+                #Storing new repositories in new variable
+                $new_repos = ($comparison | Where-Object {($_.SideIndicator -match "=>")}).InputObject
+
+                #Working on existing directories
+                if ($existing_repos -ne $Null) {
+
+                    #Finding total existing directories
+                    $total_repos = ($existing_repos | Measure-Object).count
+                    $number = 1
+                    foreach ($existing_repo in $existing_repos) {
+                        Set-location -Path .\$($existing_repo.name)
+
+                        #Checking each directory to see if a .git folder exists (hidden or not)
+                        if (($(Get-ChildItem -Hidden) -match ".git") -or ($(Get-ChildItem) -match ".git")) {
+                            Write-Host "Working on repository $($existing_repo.name). Number $number of $total_repos" -ForegroundColor Red -BackgroundColor White
+                            git pull --all 
+                            Set-Location ..
+                            $number++   
+                        }
+                        else {
+                            Write-Host "This is not a git repository" -ForegroundColor Red -BackgroundColor White
+                            Set-Location ..
+                        }
+                    }
+                }
+                
+                #Checking to see if there are any new repositories to be cloned into the directory
+                if ($new_repos -ne $Null) {
+                    $count =1
+                    $total = ($new_repos | Measure-Object).count
+                    foreach ($new_repo in $new_repos) {
+                        $clone_repo = $Repos | Where-Object {($_.name -eq $new_repo)}
+                        Write-Host "Working on new repository $($clone_repo.name).  Number $count of $total." -ForegroundColor Red -BackgroundColor White
+                        git clone $clone_repo.clone_url
+                        $count++
+                    }
+                }
+            }            
         }
         else {
-            $comparison = Compare-Object -ReferenceObject $existing_repos.name -DifferenceObject $Repos.name
-            $new_repos = ($comparison | Where-Object {($_.SideIndicator -match "=>")}).InputObject
-            if ($existing_repos -ne $Null) {
-                $total_repos = ($existing_repos | Measure-Object).count
-                $number = 1
-                foreach ($existing_repo in $existing_repos) {
-                    Set-location -Path .\$($existing_repo.name)
-                    if (($(Get-ChildItem -Hidden) -match ".git") -or ($(Get-ChildItem) -match ".git")) {
-                        Write-Host "Working on repository $($existing_repo.name). Number $number of $total_repos" -foregroundcolor Red
-                        git pull --all 
-                        Set-Location ..
-                        $number++   
-                    }
-                    else {
-                        Write-Host "This is not a git repository" -ForegroundColor Red
-                        Set-Location ..
-                    }
-                }
-            }
-            if ($new_repos -ne $Null) {
-                foreach ($new_repo in $new_repos) {
-                    $clone_url = ($Repos | Where-Object {($_.name -eq $new_repo)}).clone_url
-                    git clone $clone_url
-                }
-            }
         }
     }
     End
     {
     }
 }
+
+#Execute cmdlet
+GitHub-Clone
