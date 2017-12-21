c9509c55d9c99dc64e93023e507061c61dcd7409
CaptainFluffyToes <DJKhan85@gmail.com>
2017-12-20 20:23:12 -0500

    Updated logic that handles existing files and new files.  Changed the get-childitem to only find directories and added a check to make sure we are dealing with a git repo.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # powershell_github_clone
-This powershell function will clone all repos present in a github account.  This is useful if there are multiple repos in a single account.  It will also update the local copy to the newest version.
+## Description
+This powershell function's main purpose is to keep a local directory updated with all of an accounts repositories.  It has some built in saftey features:
+
+* Clones all new repositories at the same level as any existing repository
+* Checks existing directories to make sure they are git repositories
+* Runs git pull --all on any existing repositories
+
+## Usage
+The .ps1 file is not self executing.  You will have to add 'GitHub-Clone' to the end of the file if you would like it to execute without any other commands.  otherwise you can run:
+
+* powershell "github-clone.ps1"
+
+The ps1 file can also be added to your user powershell file so that the cmdlet is always available.
--- github_clone.ps1
+++ github_clone.ps1
@@ -1,22 +1,10 @@
 <#
 .Synopsis
-   This cmdlet will clone an entire github account into the local machine
+   This cmdlet is designed to make your life easier with github repositories.
 .DESCRIPTION
-   Long description
+   This cmdlet will get a full listing of repositories for the specified account by connecting to the GitHub api to pull down the repos.  It will then check the existing directory to see if there are any existing repositories and perform a git pull --all to fetch the latest changes.  It will clone any non existing repos into the same directory.
 .EXAMPLE
-   Example of how to use this cmdlet
-.EXAMPLE
-   Another example of how to use this cmdlet
-.INPUTS
-   The input of this cmdlet is the 
-.OUTPUTS
-   Output from this cmdlet (if any)
-.NOTES
-   General notes
-.COMPONENT
-   The component this cmdlet belongs to
-.ROLE
-   The role this cmdlet belongs to
+   GitHub-Clone -Account "CaptainFluffyToes"
 .FUNCTIONALITY
    The functionality that best describes this cmdlet
 #>
@@ -29,20 +17,13 @@ function GitHub-Clone
     [OutputType([String])]
     Param
     (
-        # This parameter is the location of the repos that are going to be used.
+        # This parameter is the account of the repos that are going to be used.
         [Parameter(Mandatory=$false, 
                    ValueFromPipeline=$true,
                    ValueFromPipelineByPropertyName=$true, 
                    ValueFromRemainingArguments=$false, 
                    Position=0)]
-        [string]$Account,
-
-        [Parameter(Mandatory=$false, 
-        ValueFromPipeline=$true,
-        ValueFromPipelineByPropertyName=$true, 
-        ValueFromRemainingArguments=$false, 
-        Position=1)]
-        [array]$Repos
+        [string]$Account
     )
 
     Begin
@@ -54,55 +35,44 @@ function GitHub-Clone
             }
         } while ($Account -eq $Null)
         if ($Repos -eq $Null) {
-            [array]$Repos = @(
-                "chef_roles",
-                "chef_jenkins_configuration",
-                "chef_conjur_configuration",
-                "chef_base_configuration",
-                "chef_plex_configuration",
-                "chef_media_configuration",
-                "chef_docker_configuration",
-                "bash_chefclient",
-                "docker_media_nzbget",
-                "docker_media_plexpy",
-                "docker_media_couchpotato",
-                "docker_media_sabnzbd",
-                "docker_media_sonarr",
-                "docker_media_plex",
-                "docker_admin_jenkins",
-                "conjur_admin_policy",
-                "powershell_github_clone",
-                "docker_network_unifi",
-                "powershell_iits",
-                "demo_conjur_jenkins",
-                "demo_aim_jenkins",
-                "VMwareBackup"
-            )            
+            $Repos = Invoke-RestMethod -Method GET -Uri "https://api.github.com/users/$Account/repos"
         }
     }
     Process
     {
-        [array]$existing_dir = Get-ChildItem
-        if ($existing_dir -eq $Null) {
+        [array]$existing_repos = Get-ChildItem -Directory
+        if ($existing_repos -eq $Null) {
+            $count = 1
             foreach ($new_repo in $Repos) {
-                git clone "https://github.com/$Account/$new_repo.git"
+                Write-Host "Creating new repository $($new_repo.name). Number $count of $(($Repos.name | Measure-Object).count)" -ForegroundColor Red
+                git clone "$($new_repo.clone_url)"
+                $count++
             }
         }
         else {
-            $comparison = Compare-Object -ReferenceObject $existing_dir.name -DifferenceObject $Repos -IncludeEqual
-            $existing_repos = $($comparison | Where-Object {($_.SideIndicator -match "==")}).InputObject
-            if ($existing_repos -eq $Null) {
-                Write-Verbose -Message 'No existing repositories' 
-            }
-            else {
+            $comparison = Compare-Object -ReferenceObject $existing_repos.name -DifferenceObject $Repos.name
+            $new_repos = ($comparison | Where-Object {($_.SideIndicator -match "=>")}).InputObject
+            if ($existing_repos -ne $Null) {
                 $total_repos = ($existing_repos | Measure-Object).count
                 $number = 1
                 foreach ($existing_repo in $existing_repos) {
-                    Set-location -Path .\$existing_repo
-                    Write-Host "Working on repository $existing_repo. Number $number of $total_repos" -foregroundcolor Red
-                    git pull --all 
-                    Set-Location ..
-                    $number++
+                    Set-location -Path .\$($existing_repo.name)
+                    if (($(Get-ChildItem -Hidden) -match ".git") -or ($(Get-ChildItem) -match ".git")) {
+                        Write-Host "Working on repository $($existing_repo.name). Number $number of $total_repos" -foregroundcolor Red
+                        git pull --all 
+                        Set-Location ..
+                        $number++   
+                    }
+                    else {
+                        Write-Host "This is not a git repository" -ForegroundColor Red
+                        Set-Location ..
+                    }
+                }
+            }
+            if ($new_repos -ne $Null) {
+                foreach ($new_repo in $new_repos) {
+                    $clone_url = ($Repos | Where-Object {($_.name -eq $new_repo)}).clone_url
+                    git clone $clone_url
                 }
             }
         }
